09fc4452ad3ad4e54e11baf28ca4916d2772f037
Ubuntu <ubuntu@ip-172-31-6-50.ap-northeast-2.compute.internal>
2019-09-06 11:16:56 +0000

    오타 수정

--- tail.sh
+++ tail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIR_NAME=programs
-if [ $1 = "elasticsearch" ] || [ $1 = "logstash" ] || [ $1 = "kibana" ] || [ $1 = "filebeat" ] || [ $1 = "metricbeat" ] ] ; then
+if [ $1 = "elasticsearch" ] || [ $1 = "logstash" ] || [ $1 = "kibana" ] || [ $1 = "filebeat" ] || [ $1 = "metricbeat" ] ; then
   tail -n 100 -f $ELASTICSTACK_HOME/$DIR_NAME/$1/$1.log
 else
   echo "invalid argument : $1"
